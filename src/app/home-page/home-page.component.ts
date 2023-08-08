a6b38d567d6e32c48e0a780f28571cf45a51922b
SimonaGancheva <ilievas95@gmail.com>
2023-08-08 12:35:48 +0300

    Setup home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/api.service';
+import { UserService } from '../user/user.service';
+import { Project } from '../types/project';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
+  projectList: Project[] = [];
 
+  constructor(
+    private apiService: ApiService,
+    private userService: UserService
+  ) {}
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.apiService.getProjects(4).subscribe({
+      next: (projects) => {
+        this.projectList = projects;
+      },
+      error: (err) => {
+        console.error(`Error: ${err}`);
+      },
+    });
+  }
 }
