146a2bde80e4e526d19598be5ac1377207b3483d
SimonaGancheva <ilievas95@gmail.com>
2023-08-12 12:24:16 +0300

    Modify Rest-api to overcome CORS

--- Rest-api/index.js
+++ Rest-api/index.js
@@ -26,8 +26,8 @@ dbConnector()
 
     app.use(function(req, res, next) {
       res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Origin');
       res.setHeader('Access-Control-Allow-Credentials', true);
       next();
     });
